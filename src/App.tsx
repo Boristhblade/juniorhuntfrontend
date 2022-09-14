e4cad102af211f6cbb31c56049b29142b0a3d589
Boris <watasinonamaewa@gmail.com>
2022-09-14 16:05:54 +0400

    re-init project with cra-template-typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react'
+import './App.css'
 
-export const App = () => {
+export const App: React.FC = () => {
   return (
-    <div />
-  );
+    <div className="App">
+    </div>
+  )
 }
+
+export default App
