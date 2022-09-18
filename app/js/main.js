4e58c46e7690e9ef633f946752759074f0fd8c83
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2022-09-18 11:57:38 +0300

    adaptive+folder with optimize project

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,36 @@
 $(function () {
+  $('.cards-side__title').on('click', function () {
+    let is_active = false
 
+    if ($(this).hasClass('cards-side__title--active')) {
+      is_active = true
+    }
 
-    $(".cards-side__title").on("click",function(){
-        let is_active = false;
-        
-       if( $(this).hasClass('cards-side__title--active') ){ is_active = true; }
-        
-       $(this).removeClass('cards-side__title--active');
-       
-     if(!is_active){  $(this).addClass('cards-side__title--active');  }
+    $(this).removeClass('cards-side__title--active')
+
+    if (!is_active) {
+      $(this).addClass('cards-side__title--active');
+    }
+    // $(this).toggle()
     
-    
-        });
+   
+  })
+  // $('.btn-show').on('click',function(){
+  //   $('.cards-side__hidden').toggle()
+  // })
+
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 43,
+    max: 68,
+    values: [ 43, 68 ],
+    slide: function( event, ui ) {
+      $( "#amount1" ).text( "€" + ui.values[ 0 ]+"K")
+      $("#amount2").text( "€" + ui.values[ 1 ] +"K");
+    }
+  });
+  $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+    " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  
+
 })
