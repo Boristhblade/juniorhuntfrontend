e21ec1d3cf001a9a66be0d1dffa595a63c0402de
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2022-09-20 19:24:01 +0300

    btn sort

--- app/js/main.js
+++ app/js/main.js
@@ -9,28 +9,43 @@ $(function () {
     $(this).removeClass('cards-side__title--active')
 
     if (!is_active) {
-      $(this).addClass('cards-side__title--active');
+      $(this).addClass('cards-side__title--active')
     }
     // $(this).toggle()
-    
-   
   })
   // $('.btn-show').on('click',function(){
   //   $('.cards-side__hidden').toggle()
   // })
+  $('.cards-content__sort').on('click', function () {
+    if ($('.cards-content__hidden').hasClass('cards-content__hidden--active')) {
+      $('.cards-content__hidden').removeClass('cards-content__hidden--active')
+    } else {
+      $('.cards-content__hidden').addClass('cards-content__hidden--active')
+    }
+  })
+  $('.cards-content__hidden-link').on('click', function () {
+    $('.cards-content__hidden').removeClass('cards-content__hidden--active')
+    console.log(1)
+  })
 
-  $( "#slider-range" ).slider({
+  $('.cards-content__sort-text').on('click', function () {
+    $(this).toggleClass('transform')
+  })
+
+  $('#slider-range').slider({
     range: true,
     min: 43,
     max: 68,
-    values: [ 43, 68 ],
-    slide: function( event, ui ) {
-      $( "#amount1" ).text( "€" + ui.values[ 0 ]+"K")
-      $("#amount2").text( "€" + ui.values[ 1 ] +"K");
-    }
-  });
-  $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-    " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-  
-
+    values: [43, 68],
+    slide: function (event, ui) {
+      $('#amount1').text('€' + ui.values[0] + 'K')
+      $('#amount2').text('€' + ui.values[1] + 'K')
+    },
+  })
+  $('#amount').val(
+    '$' +
+      $('#slider-range').slider('values', 0) +
+      ' - $' +
+      $('#slider-range').slider('values', 1)
+  )
 })
